96933115a52ebc084b0d16e084ff3690e4c33639
denver daniels <denver@projectcodex.co>
2016-06-29 15:11:07 +0200

    database errors of travis fixed

--- tables.sql
+++ tables.sql
@@ -4,14 +4,6 @@ create table users(
   password varchar(100)
 );
 
-create table music(
-  id int auto_increment primary key,
-  song varchar(100),
-  name varchar(100),
-  user_id int not null,
-  foreign key(user_id) references users (id)
-);
-
 insert into users(username, password) values('bobby', '$2a$10$1OPvghAQXdfdf7NzhejO3wuUQFnSAkyQiLVJPewH51HAiPmwxMHFY6');
 insert into users(username, password) values('nikki', '$2a$10$1OPvghAQXTKf7NzhejO3wuUQFnSAkyQiLVJPewH51HAiPmwxMHFY6');
 insert into users(username, password) values('bella', '$2a$10$1OfdghAQXTKf7NzhejO3wuUQFnSAkyQiLVJPewH51HAiPmwxMHFY6');
@@ -20,6 +12,13 @@ insert into users(username, password) values('poppy', '$2a$10$1OPvghAQnfkbvf7Nzh
 insert into users(username, password) values('dicky', '$2a$10$1OPvghAQnfkbvf7NzhejO3wuUQFnSAkyQiLVJPewH51HAiPmwxMHFY6');
 insert into users(username, password) values('erl', '$2a$10$1OPvghAQnfkbvf7NzhejO3wuUQFnSAkyQiLVJPewH51HAiPmwxMHFY6');
 
+create table music(
+  id int auto_increment primary key,
+  song varchar(100),
+  name varchar(100),
+  user_id int not null,
+  foreign key(user_id) references users (id)
+);
 
 insert into music(song, name, user_id) values('/uploads/jh28t24bbc8bw8we', 'song.mp3',  1);
 insert into music(song, name, user_id) values('/uploads/bfjdbfn37ebffufb', 'song1.mp3', 2);
