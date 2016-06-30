65f584f095102fa3639fccf8f57f6de238e5df6b
denver daniels <denver@projectcodex.co>
2016-06-30 14:10:10 +0200

    new service added

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express'),
-      exphbs   = require('express-handlebars'),
-      bodyParser = require('body-parser'),
-      multer =  require('multer'),
-      mysql = require('mysql'),
-      myConnection = require('express-myconnection'),
-      app = express();
+const express      = require('express'),
+      exhbs        = require('express-handlebars'),
+      cookieParser = require('cookie-parser'),
+      session      = require('express-session'),
+      bodyParser   = require('body-parser'),
+      mysql        = require('mysql'),
+      multer       = require('multer'),
+      connectionPv = require('connection-provider'),
+      compression  = require('compression'),
+      flash        = require('express-flash'),
+      app          = express();
 
-const music = require('./routes/index');
-// const extention = .mp3;
+const music = require('./routes/music');
+const login = require('./routes/login');
+const MusicDataService = require('./data_services/musicDataService');
+const QueryDataService = require('./data_services/queryDataService');
+const LoginDataService = require('./data_services/loginDataService');
 const dbOptions = {
   host: 'localhost',
   port: 3306,
@@ -16,19 +23,35 @@ const dbOptions = {
   database: 'music'
 };
 
-app.use(myConnection(mysql, dbOptions, 'single'));
+const serviceSetupCallBack = function (connection) {
+  return {
+    queryDataService                  : new QueryDataService(connection),
+    musicDataService                  : new MusicDataService(connection),
+    loginDataService                  : new LoginDataService(connection)
+  }
+};
+
+app.use(connectionPv(dbOptions, serviceSetupCallBack));
+app.use(cookieParser('shhhh, very secret'));
+app.use(session({ secret : 'keyboard cat', cookie :{ maxAge : 3600000 }, resave : true, saveUninitialized : true }));
 app.use(express.static('public'));
-app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+app.use(compression());
+app.use(flash());
+app.engine('handlebars', exhbs({defaultLayout : 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('login',{
+    layout: false
+  });
 });
 app.get('/music', music.show);
 app.get('/add', function(req, res){
   res.render('add');
 });
-
+app.post('/login', login.login);
 app.post('/music/upload',multer({ dest: './public/uploads/', ext: '.mp3'}).single('audio') , music.add);
 app.get('/delete/:id', music.delete);
 const port = process.env.PORT || 2016;
