ac534a88fce0cfdca4bc56a06cdd9ef8fd72f8bf
denver daniels <denver@projectcodex.co>
2016-07-05 10:06:17 +0200

    missing module added

--- routes/signup.js
+++ routes/signup.js
@@ -1,4 +1,5 @@
 const encryptonator = require('encryptonator');
+const co = require('co');
 
 exports.add = function(req, res, next) {
     co(function*() {
@@ -7,9 +8,16 @@ exports.add = function(req, res, next) {
         const data = {
             username: req.body.username
         };
+        const password = req.body.password;
+        const confirmPassword = req.body.confirm;
+        if(confirmPassword !== password){
+          req.flash('alert', 'passwords does not match');
+          return res.redirect('/signup');
+        } else {
         const hash = yield encryptonator.encryptPassword(req.body.password);
         data.password = hash;
         const signup = yield signupDataService.add(data);
+      };
         try {
             res.redirect('/');
         } catch (err) {
