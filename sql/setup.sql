70a7f6bf8dd83b2d54deccb2c2c90c41a17c2ab5
denver daniels <denver@projectcodex.co>
2016-07-30 09:04:48 +0200

    database setup script

---
+++ sql/setup.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE music;
+CREATE USER admin@localhost IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON music.* TO admin@localhost;
+FLUSH PRIVILEGES;
+
+USE music;
+
+CREATE TABLE users(
+  id int auto_increment primary key,
+  username varchar(100),
+  password varchar(100),
+);
+
+CREATE TABLE music(
+  id int auto_increment primary key,
+  song varchar(100),
+  name varchar(100),
+  user_id int not null,
+  foreign key(user_id) references users(id)
+);
+
+CREATE TABLE videos(
+  id int auto_increment primary key,
+  video varchar(100),
+  name varchar(100),
+  user_id int not null,
+  foreign key(user_id) references users(id)
+);
