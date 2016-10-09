c6aba0009a31e19e6342843dea54fd670b11f34d
denver daniels <denver@projectcodex.co>
2016-10-09 10:27:01 +0200

    password reset route

---
+++ routes/forgotPassword.js
@@ -0,0 +1,21 @@
+const uuid = require('uuid'),
+    co = require('co');
+
+exports.reset = function(req, res, next) {
+    co(function*() {
+            const data = {
+                email: req.body.email,
+            };
+            const services = yield req.getServices();
+            const resetDataService = services.resetDataService;
+            const reserPassword = yield resetDataService.send(data);
+
+        }
+        try {
+            res.redirect('/');
+        } catch (err) {
+            console.log(err);
+            res.render('/error');
+        };
+    });
+};
